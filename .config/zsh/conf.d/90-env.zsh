1960bfdddf2f49a08e5569e4fd668cf61c814b3b
Tim Roes <mail@timroes.de>
2021-10-14 17:06:38 +0200

    Add CI stats opt-out

--- .config/zsh/conf.d/90-env.zsh
+++ .config/zsh/conf.d/90-env.zsh
@@ -5,4 +5,6 @@ if [ $? -eq 0 ]; then
 	export JAVA_HOME="/usr/lib/jvm/$(archlinux-java get)/"
 fi
 
+# opting out of CI stat collection of the Kibana repo
+export CI_STATS_DISABLED=true
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
