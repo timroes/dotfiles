2b7774da7cf7500d17870328c711215e5c6df5c7
Tim Roes <mail@timroes.de>
2022-01-17 15:25:48 +0100

    Change env variables

--- .config/zsh/conf.d/90-env.zsh
+++ .config/zsh/conf.d/90-env.zsh
@@ -5,6 +5,5 @@ if [ $? -eq 0 ]; then
 	export JAVA_HOME="/usr/lib/jvm/$(archlinux-java get)/"
 fi
 
-# opting out of CI stat collection of the Kibana repo
-export CI_STATS_DISABLED=true
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+export AIRBYTE_ROLE=airbyter
