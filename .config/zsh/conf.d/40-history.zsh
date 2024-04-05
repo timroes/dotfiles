f8f807ffcd1e305a67dbd065e6138b6ef636cd40
Tim Roes <mail@timroes.de>
2024-04-05 20:19:53 +0200

    improve zsh config

--- .config/zsh/conf.d/40-history.zsh
+++ .config/zsh/conf.d/40-history.zsh
@@ -7,3 +7,12 @@ setopt HIST_IGNORE_DUPS
 setopt HIST_SAVE_NO_DUPS
 setopt HIST_EXPIRE_DUPS_FIRST
 setopt SHARE_HISTORY
+
+HISTORY_IGNORE="(jwt decode *)"
+
+zshaddhistory() {
+  emulate -L zsh
+  ## uncomment if HISTORY_IGNORE should use EXTENDED_GLOB syntax
+  # setopt extendedglob
+  [[ $1 != ${~HISTORY_IGNORE} ]]
+}
